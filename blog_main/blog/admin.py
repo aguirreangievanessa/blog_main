e90ea9f90b5e984858f4003c4a9670f28f8f402d
aguirreangievanessa <agieeevnss@gmail.com>
2026-03-20 23:28:19 -0700

    now

---
+++ blog_main/blog/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Post, Comment, Category
+
+class CommentItemInline(admin.TabularInline):  # corregido nombre
+    model = Comment
+    raw_id_fields = ['post']  # corregido campo
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'intro', 'body']
+    list_display = ('title', 'category', 'created_at', 'status')
+    list_filter = ['category', 'created_at', 'status']
+    inlines = [CommentItemInline]
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ('title',)  # tupla correcta
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'created_at')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
